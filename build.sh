c81fc8a4ada7b72ce993f63018b149fe10526659
MVDW <maxvdwolf@gmail.com>
2022-10-13 16:11:18 +0200

    install requirement
    tested under debian 11

--- build.sh
+++ build.sh
@@ -6,6 +6,20 @@
 # This tool is made to create a full SplashOS install.
 #  
 
+# check if root perms.
+if [[ "$EUID" = 0 ]]; then
+    echo "(1) already root"
+else
+    sudo -k # make sure to ask for password on next sudo
+    if sudo true; then
+        echo "(2) correct password"
+    else
+        echo "(3) wrong password"
+        exit 1
+    fi
+fi
 
 echo "Check dependencies..."
-source ./build_env/version_check.sh
+
+sudo ./build_env/prepare_scripts/install_requirements.sh
+sudo ./build_env/prepare_scripts/version_check.sh
---
+++ build_env/prepare_scripts/install_requirements.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+rm /bin/sh
+ln -s /bin/bash /bin/sh
+apt install binutils bison gawk gcc g++ make patch texinfo -y
+
