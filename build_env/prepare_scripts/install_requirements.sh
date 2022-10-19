6ee6baa93ed58abcfb5f8d224aa93d2f4e2dd8de
MVDW <maxvdwolf@gmail.com>
2022-10-19 20:46:09 +0200

    Update install_requirements.sh

--- build_env/prepare_scripts/install_requirements.sh
+++ build_env/prepare_scripts/install_requirements.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo rm /bin/sh
 sudo ln -s /bin/bash /bin/sh
-sudo apt install binutils bison gawk gcc g++ make patch texinfo -y
+sudo apt install binutils bison gawk gcc g++ make patch texinfo libisl-dev -y
 
