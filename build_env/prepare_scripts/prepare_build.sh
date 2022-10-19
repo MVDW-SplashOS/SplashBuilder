d38d8f37a397cada9333bcac2a02066666534b92
MVDW <maxvdwolf@gmail.com>
2022-10-19 19:16:26 +0200

    Update prepare_build.sh

--- build_env/prepare_scripts/prepare_build.sh
+++ build_env/prepare_scripts/prepare_build.sh
@@ -51,45 +51,49 @@ for TOOL in ${config_tools_enabled_[*]}; do
 done
 
 
-mkdir -pv $LFS/{bin,etc,lib,sbin,usr,var,lib64,tools}
+
+mkdir -pv $LFS/{etc,var,lib64} $LFS/usr/{bin,lib,sbin}
+
+for i in bin lib sbin; do
+	ln -sv usr/$i $LFS/$i
+done
+mkdir -pv $LFS/tools
+
+
 
 if ! test $(id -u distbuild) ; then
 
-groupadd distbuild
-useradd -s /bin/bash -g distbuild -m -k /dev/null distbuild
-passwd distbuild
+	groupadd distbuild
+	useradd -s /bin/bash -g distbuild -m -k /dev/null distbuild
+	passwd distbuild
 
 
-echo "distbuild ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/sudoers_distbuild
+	echo "distbuild ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/sudoers_distbuild
 
-dbhome=$(eval echo "~distbuild")
+	dbhome=$(eval echo "~distbuild")
 
-cat > $dbhome/.bash_profile << "EOF"
-exec env -i HOME=$HOME TERM=$TERM PS1='\u:\w\$ ' /bin/bash
-EOF
+	cat > $dbhome/.bash_profile << "EOF"
+		exec env -i HOME=$HOME TERM=$TERM PS1='\u:\w\$ ' /bin/bash
+	EOF
 
-cat > $dbhome/.bashrc << EOF
-set +h
-umask 022
-LFS=$LFS
-export DIST_ROOT=$DIST_ROOT
-EOF
-
-cat >> $dbhome/.bashrc << "EOF"
-LC_ALL=POSIX
-LFS_TGT=$(uname -m)-lfs-linux-gnu
-PATH=/usr/bin
-if [ ! -L /bin ]; then PATH=/bin:$PATH; fi
-PATH=$LFS/tools/bin:$PATH
-CONFIG_SITE=$LFS/usr/share/config.site
-export LFS LC_ALL LFS_TGT PATH CONFIG_SITE
-export MAKEFLAGS="-j$(nproc)"
-EOF
+	cat > $dbhome/.bashrc << EOF
+		set +h
+		umask 022
+		LFS=$LFS
+		export DIST_ROOT=$DIST_ROOT
+		LC_ALL=POSIX
+		LFS_TGT=$(uname -m)-lfs-linux-gnu
+		PATH=/usr/bin
+		if [ ! -L /bin ]; then PATH=/bin:$PATH; fi
+		PATH=$LFS/tools/bin:$PATH
+		CONFIG_SITE=$LFS/usr/share/config.site
+		export LFS LC_ALL LFS_TGT PATH CONFIG_SITE
+	EOF
 
 
 fi
 
-chown -v distbuild $LFS/{usr,lib,var,etc,bin,sbin,tools,lib64}
+chown -v lfs $LFS/{usr{,/*},lib,var,etc,bin,sbin,tools,lib64}
 chown -v distbuild $DIST_ROOT/build_env/sources
 
 echo "Done!" 
