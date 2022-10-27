aef17f036873fa2a59abc8b75e4994443934ca48
MVDW <maxvdwolf@gmail.com>
2022-10-27 13:56:20 +0200

    add support for Fedora pre install

--- build_env/prepare_scripts/install_requirements.sh
+++ build_env/prepare_scripts/install_requirements.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 sudo rm /bin/sh
 sudo ln -s /bin/bash /bin/sh
-sudo apt install binutils bison gawk gcc g++ make patch texinfo libisl-dev -y
 
+if [ -x "$(command -v apt-get)" ]; then sudo apt-get install binutils bison gawk gcc g++ make patch texinfo libisl-dev -y
+elif [ -x "$(command -v dnf)" ];     then sudo dnf install binutils bison gawk gcc g++ make patch texinfo -y
+else echo "FAILED TO INSTALL PACKAGES: Package manager not found.">&2; fi
