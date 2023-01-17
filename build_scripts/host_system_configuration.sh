b181ac191084f42f7537dbebcfe54f889ac4b818
MVDW <maxvdwolf@gmail.com>
2023-01-17 16:44:11 +0100

    Update host_system_configuration.sh

--- build_scripts/host_system_configuration.sh
+++ build_scripts/host_system_configuration.sh
@@ -26,51 +26,55 @@ else
 fi
 unset MYSH
 
+# TODO: install when needed, do more checks
+
+# 
+if [ -x "$(command -v apt-get)" ]; then sudo apt-get install binutils bison gawk gcc g++ make patch texinfo libisl-dev -y
+elif [ -x "$(command -v dnf)" ];     then sudo dnf install binutils bison gawk gcc g++ make patch texinfo -y
+else echo "FAILED TO INSTALL PACKAGES: Package manager not found.">&2; fi
 
 
+echo -n "Binutils: "; ld --version | head -n1 | cut -d" " -f3-
+bison --version | head -n1
 
+if [ -h /usr/bin/yacc ]; then
+  echo "/usr/bin/yacc -> `readlink -f /usr/bin/yacc`";
+elif [ -x /usr/bin/yacc ]; then
+  echo yacc is `/usr/bin/yacc --version | head -n1`
+else
+  echo "yacc not found"
+fi
 
-#echo -n "Binutils: "; ld --version | head -n1 | cut -d" " -f3-
-#bison --version | head -n1
+echo -n "Coreutils: "; chown --version | head -n1 | cut -d")" -f2
+diff --version | head -n1
+find --version | head -n1
+gawk --version | head -n1
 
-#if [ -h /usr/bin/yacc ]; then
-#  echo "/usr/bin/yacc -> `readlink -f /usr/bin/yacc`";
-#elif [ -x /usr/bin/yacc ]; then
-#  echo yacc is `/usr/bin/yacc --version | head -n1`
-#else
-#  echo "yacc not found"
-#fi
+if [ -h /usr/bin/awk ]; then
+  echo "/usr/bin/awk -> `readlink -f /usr/bin/awk`";
+elif [ -x /usr/bin/awk ]; then
+  echo awk is `/usr/bin/awk --version | head -n1`
+else
+  echo "awk not found"
+fi
 
-#echo -n "Coreutils: "; chown --version | head -n1 | cut -d")" -f2
-#diff --version | head -n1
-#find --version | head -n1
-#gawk --version | head -n1
+gcc --version | head -n1
+g++ --version | head -n1
+grep --version | head -n1
+gzip --version | head -n1
+cat /proc/version
+m4 --version | head -n1
+make --version | head -n1
+patch --version | head -n1
+echo Perl `perl -V:version`
+python3 --version
+sed --version | head -n1
+tar --version | head -n1
+makeinfo --version | head -n1  # texinfo version
+xz --version | head -n1
 
-#if [ -h /usr/bin/awk ]; then
-#  echo "/usr/bin/awk -> `readlink -f /usr/bin/awk`";
-#elif [ -x /usr/bin/awk ]; then
-#  echo awk is `/usr/bin/awk --version | head -n1`
-#else
-#  echo "awk not found"
-#fi
-
-#gcc --version | head -n1
-#g++ --version | head -n1
-#grep --version | head -n1
-#gzip --version | head -n1
-#cat /proc/version
-#m4 --version | head -n1
-#make --version | head -n1
-#patch --version | head -n1
-#echo Perl `perl -V:version`
-#python3 --version
-#sed --version | head -n1
-#tar --version | head -n1
-#makeinfo --version | head -n1  # texinfo version
-#xz --version | head -n1
-
-#echo 'int main(){}' > dummy.c && g++ -o dummy dummy.c
-#if [ -x dummy ]
-#  then echo "g++ compilation OK";
-#  else echo "g++ compilation failed"; fi
-#rm -f dummy.c dummy
+echo 'int main(){}' > dummy.c && g++ -o dummy dummy.c
+if [ -x dummy ]
+  then echo "g++ compilation OK";
+  else echo "g++ compilation failed"; fi
+rm -f dummy.c dummy
