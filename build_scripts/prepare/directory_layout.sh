dfa73524486f681520c092e40bb7955a6d3c20b8
MVDW <maxvdwolf@gmail.com>
2023-02-23 19:23:53 +0100

    Add checks if symlink exist for splash_partition_root

--- build_scripts/prepare/directory_layout.sh
+++ build_scripts/prepare/directory_layout.sh
@@ -3,6 +3,10 @@
 mkdir -pv $splash_partition_root/{etc,var,lib64,tools} $splash_partition_root/usr/{bin,lib,sbin}
 
 for i in bin lib sbin; do
-  ln -sv usr/$i $splash_partition_root/$i
+	if ! [[ -L "$splash_partition_root/$i" ]]; then
+		ln -sv "usr/$i" "$splash_partition_root/$i"
+	else
+		echo "Symlink '$splash_partition_root/$i' already exist, skipping.."
+	fi
 done
 
