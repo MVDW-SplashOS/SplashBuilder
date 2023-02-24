07ef8f81e905f0659b54926134d583901428d314
MVDW <maxvdwolf@gmail.com>
2023-02-24 15:35:05 +0100

    make buildscript only run as spesific user

--- build_splashos.sh
+++ build_splashos.sh
@@ -14,15 +14,16 @@ source ./build_scripts/style/color.sh
 
 set -e
 
-# check if root perms.
-if [ "$EUID" -ne 0 ]
-then
-	printf $BRed
+# check if correct user is running script.
+if [ $USER != "splashbuilder" ]; then
+        printf $BRed
 	printf "ERROR: "
 	printf $IRed
-	printf "Please run this as root or with sudo\n"
+	printf "Please run this script as splashbuilder.\n"
 	exit 2
 fi
+
+
 
 # Print SplashOS builder info
 source ./build_scripts/style/print_logo.sh
@@ -44,17 +45,6 @@ export LC_ALL=C
 
 # All steps required to build SplashOS:
 # 
-# Step 1: Prepare the host system
-#     |
-#     |-- 1.0 - Check, install and configure host system
-#     |
-#     |-- 1.1 - Make partitions
-#     |
-#     |-- 1.2 - Download dependencies
-#     |
-#     |-- 1.3 - Create directory layout
-#     |
-#     |-- 1.4 - Setup bash enviroment
 #
 #
 # Step 2:
@@ -65,54 +55,10 @@ export LC_ALL=C
 
 # -------------------------------------------------------------
 #
-#      STEP 1: Prepare the host system
-#
-# -------------------------------------------------------------
-
-# Step 1.0 - Check, install and configure host system
-source ./build_scripts/prepare/system_requirements.sh
-
-# Step 1.1 - Make partitions
-source ./build_scripts/prepare/partitions.sh
-
-# Step 1.2 - Download dependencies
-source ./build_scripts/prepare/dependencies.sh
-
-# Step 1.3 - Create directory layout
-source ./build_scripts/prepare/directory_layout.sh
-
-# Step 1.4 - Create and setup SplashBuilder user
-source ./build_scripts/prepare/create_user.sh
-
-
-
-test="fail"
-su splashbuilder <<EOSU
-echo 'test ${test} ${splash_partition_root} :: $(whoami)'
-
-EOSU
-
-exit;
-#su -l splashbuilder -c 'test -n "${splash_partition_root}" || { echo "splash_partition_root is not set for user $(whoami) : $splash_partition_root"; exit 1; }; echo "${splash_partition_root}"' 
-su -s /bin/bash -c 'test -n "$SPLASHOS_TGT" || { echo "SPLASHOS_TGT is not set $(whoami)"; exit 1; }; echo "$SPLASHOS_TGT"' splashbuilder
-
-
-
-
-export yaml_file=./config.yml
-export yaml_prefix="config_"
-source ./build_scripts/parse_yaml.sh
-create_variables 
-
-
-
-
-
-# -------------------------------------------------------------
-#
 #      STEP 2: Compiling a cross-toolchain
 #
 # -------------------------------------------------------------
+
 
 bash -e build_scripts/build/binutils-pass-1.sh "binutils-${config_tools_list__binutils__version}.tar.xz"
 bash -e build_scripts/build/gcc-pass-1.sh "gcc-${config_tools_list__gcc__version}.tar.xz"
