02272f389883704e188d818ba251499ef78cfd4d
BruddaCode <Fabio.wolthuis@gmail.com>
2023-03-21 15:28:30 +0100

    update buildscripts

--- build_scripts/build/linux-headers.sh
+++ build_scripts/build/linux-headers.sh
@@ -3,7 +3,7 @@ set -e
 echo ${splash_partition_root:?}
 echo ${SPLASHOS_TGT:?}
 echo ${DIST_ROOT:?}
-pwd 
+
 
 . $DIST_ROOT/build_scripts/build/inc-start.sh $1 $(basename $0)
 
@@ -12,5 +12,5 @@ make mrproper
 make headers
 find usr/include -type f ! -name '*.h' -delete
 cp -rv usr/include $splash_partition_root/usr
-pwd
+
 . $DIST_ROOT/build_scripts/build/inc-start.sh $1 $(basename $0)
