9a1548fb47005013c50283b8268a85811f2d8fbd
MVDW <maxvdwolf@gmail.com>
2023-04-04 17:30:56 +0200

    add --force parameter to mv in openssl.sh

--- build_scripts/chroot/splash_builder/openssl.sh
+++ build_scripts/chroot/splash_builder/openssl.sh
@@ -12,7 +12,7 @@ sed -i '/INSTALL_LIBS/s/libcrypto.a libssl.a//' Makefile
 
 make MANSUFFIX=ssl install
 
-mv -v /usr/share/doc/openssl /usr/share/doc/openssl-3.0.8
+mv -fv /usr/share/doc/openssl /usr/share/doc/openssl-3.0.8
 cp -vfr doc/* /usr/share/doc/openssl-3.0.8
 
 . /splash_builder/inc-end.sh $1 $(basename $0)
