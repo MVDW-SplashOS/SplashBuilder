4421e897a18afac926bf75415c041d7472cf92cd
MVDW <maxvdwolf@gmail.com>
2023-04-07 19:11:52 +0200

    made dirty "fix" for libffi not being installed in the correct directory

--- build_scripts/chroot/splash_builder/libffi.sh
+++ build_scripts/chroot/splash_builder/libffi.sh
@@ -5,7 +5,15 @@
             --with-gcc-arch=native
 
 make
+make check
 make install
+
+
+# Dirty "Fix" for building error
+ln -f /usr/lib64/libffi.la /usr/lib/libffi.la
+ln -f /usr/lib64/libffi.so.8.1.2 /usr/lib/libffi.so.8.1.2
+ln -f /usr/lib64/libffi.so.8.1.2 /usr/lib/libffi.so.8 
+ln -f /usr/lib64/libffi.so.8.1.2 /usr/lib/libffi.so
 
 
 . /splash_builder/inc-end.sh $1 $(basename $0)
