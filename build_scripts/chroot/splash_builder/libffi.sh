9888c7c1c19df86e48927b662948808919c0a6f0
MVDW <maxvdwolf@gmail.com>
2023-04-08 04:00:50 +0200

    Revert dirty "fix" for linking /usr/lib/libffi.so to /usr/lib64/libffi.so

--- build_scripts/chroot/splash_builder/libffi.sh
+++ build_scripts/chroot/splash_builder/libffi.sh
@@ -9,11 +9,4 @@ make check
 make install
 
 
-# Dirty "Fix" for building error
-ln -f /usr/lib64/libffi.la /usr/lib/libffi.la
-ln -f /usr/lib64/libffi.so.8.1.2 /usr/lib/libffi.so.8.1.2
-ln -f /usr/lib64/libffi.so.8.1.2 /usr/lib/libffi.so.8 
-ln -f /usr/lib64/libffi.so.8.1.2 /usr/lib/libffi.so
-
-
 . /splash_builder/inc-end.sh $1 $(basename $0)
