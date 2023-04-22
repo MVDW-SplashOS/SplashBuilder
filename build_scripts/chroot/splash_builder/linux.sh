918e74158d11e0166bbbc6208ea35219f7fd4b73
MVDW <maxvdwolf@gmail.com>
2023-04-23 00:58:23 +0200

    Add Linux compile script

---
+++ build_scripts/chroot/splash_builder/linux.sh
@@ -0,0 +1,17 @@
+. /splash_builder/inc-start.sh $1 $(basename $0)
+
+make mrproper
+make defconfig
+make menuconfig #TODO: make a custom config file you can wget
+
+make
+make modules_install
+
+cp -iv arch/x86/boot/bzImage /boot/vmlinuz-6.1.11-splashos-v0.1dev
+cp -iv System.map /boot/System.map-6.1.11
+cp -iv .config /boot/config-6.1.11
+
+install -d /usr/share/doc/linux-6.1.11
+cp -r Documentation/* /usr/share/doc/linux-6.1.11
+
+. /splash_builder/inc-end.sh $1 $(basename $0)
