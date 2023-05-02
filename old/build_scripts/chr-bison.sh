e63082326d48a2a0b9c87adf02fdc6db514620c0
MVDW <maxvdwolf@gmail.com>
2023-05-02 16:41:08 +0200

    removed outdated code

--- old/build_scripts/chr-bison.sh
+++
@@ -1,6 +0,0 @@
-. $DIST_ROOT/build_env/build_scripts/inc-start.sh $1 $(basename $0)
-
-./configure --prefix=/usr \
-            --docdir=/usr/share/doc/bison-3.7.5 && make && make install
-        
-. $DIST_ROOT/build_env/build_scripts/inc-end.sh $1 $(basename $0)
--- old/prepare_scripts/install_requirements.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo rm /bin/sh
-sudo ln -s /bin/bash /bin/sh
-
-if [ -x "$(command -v apt-get)" ]; then sudo apt-get install binutils bison gawk gcc g++ make patch texinfo libisl-dev -y
-elif [ -x "$(command -v dnf)" ];     then sudo dnf install binutils bison gawk gcc g++ make patch texinfo -y
-else echo "FAILED TO INSTALL PACKAGES: Package manager not found.">&2; fi
