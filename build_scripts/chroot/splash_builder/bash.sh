4e67109617740a0071312edfb66d022063f0ae74
MVDW <maxvdwolf@gmail.com>
2023-05-03 13:27:51 +0200

    removed some testing stuff

--- build_scripts/chroot/splash_builder/bash.sh
+++ build_scripts/chroot/splash_builder/bash.sh
@@ -6,15 +6,15 @@
             --docdir=/usr/share/doc/bash-5.2.15
 
 make
-chown -Rv tester .
+#chown -Rv tester .
 
-su -s /usr/bin/expect tester << EOF
-set timeout -1
-spawn make tests
-expect eof
-lassign [wait] _ _ _ value
-exit $value
-EOF
+#su -s /usr/bin/expect tester << EOF
+#set timeout -1
+#spawn make tests
+#expect eof
+#lassign [wait] _ _ _ value
+#exit $value
+#EOF
 
 make install
 
